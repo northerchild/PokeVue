bfedd6f7f84661e1818fbfe5a0843c6f8ba95ffe
Northerchild <osarus-13@hotmail.com>
2018-02-02 18:30:23 -0500

    Se agraga los componentes de vista y ahora todos los pokes son visibles

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="pokedex-card">
+		<img :src="pokemon.ThumbnailImage" class="pokedex-card-img">
+		<div class="pokedex-card-details">
+			<small class="pokedex-card-details-title">#{{pokemon.number}}</small>
+			<h4 class="poxedex-card-datails_title">{{pokemon.name}}</h4>
+			<div class="pokedex-card_types">		
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default{
+		name: 'PokeCard',
+		props:{
+			pokemon:{
+				type:Object,
+				required:true
+			}
+		}
+	}
+</script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,33 @@
 <template>
-	<default-layout></default-layout>
+	<default-layout>
+		<section slot="content" class="pokedex-items">
+			<div class="row">
+				<div class="col-md-4" v-for="(item,index) in pokemon":key="index">
+					<poke-card :pokemon='item'></poke-card>
+				</div>
+			</div>
+		</section>	
+	</default-layout>
 </template>
 <script>
 	import DefaultLayout from '@/layout/DefaultLayout'
+	import {getPokemon} from '@/service/pokemon'
+	import PokeCard from '@/components/PokemonCard'
 	export default{
 		name:'HomeView',
+		data(){
+			return{
+				pokemon:[]
+			}
+		},
+		created(){
+			getPokemon().then(response =>{
+				this.pokemon = response.data
+			}).catch(err => console.log(err))		
+		},
 		components:{
-			DefaultLayout
+			DefaultLayout,
+			PokeCard
 		}
 	}
 </script>
