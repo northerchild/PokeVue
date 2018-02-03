c95eac319655d10cef8f66a2f8b166e5e6508d3d
Northerchild <osarus-13@hotmail.com>
2018-02-03 14:41:22 -0500

    Ahora en detalles se sabe que tipo es el poke y sus debilidades

--- src/views/detailPokemon/DetailPokemon.vue
+++ src/views/detailPokemon/DetailPokemon.vue
@@ -1,32 +1,55 @@
 <script>
-	import DefaultLayout from '@/layout/DefaultLayout' //eslint-disable-line no-unused-vars
-	import PokeCard from '@/components/PokemonCard'
-	export default{
-		name:'DetailPokemonView',
-		render(h){
-		let pokemon = this.$route.params.pokemon		
-		if (!pokemon) {
-			this.$router.push({ name: 'Home' })
-		}else{
-		return <DefaultLayout>
-				<section slot="content" class="pokedex-item mt-5">
-					<div className="row">
-						<div class="col-md-12">
-						 <h1 class="pokemon-datail_tittle text-center mb-5">{pokemon.name}
-						 	<span class="text-muted">#{pokemon.number}</span>
-						 </h1>
-						 <hr />
-						 <div className="row">
-						 	<div className="col-md-5">
-						 		<img class="pokedex-detalle" src={pokemon.ThumbnailImage}/>
-						 	</div>
-						 </div>
-						</div>
-					</div>
-				</section>
-			</DefaultLayout>					
-		}
-		}
-	}
+import DefaultLayout from '@/layout/DefaultLayout' // eslint-disable-line no-unused-vars
+import PokedexTag from '@/components/PokedexTag' // eslint-disable-line no-unused-vars
+export default {
+  name: 'DetailPokemonView',
+  render () {
+    let pokemon = this.$route.params.pokemon
+    if (!pokemon) {
+      this.$router.push({ name: 'Home' })
+    } else {
+      // Creating tags for types
+      const pokemonTags = () => {
+        return pokemon.type.map((type) => {
+          return <PokedexTag pokemonType={type}></PokedexTag>
+        })
+      }
+      const weaknessTags = () => {
+        return pokemon.weakness.map((type) => {
+          return <PokedexTag pokemonType={type}></PokedexTag>
+        })
+      }
+      return <DefaultLayout>
+        <section slot="content" class="pokedex-item mt-5">
+          <div class="row">
+            <div class="col-md-12">
+              <h1 class="pokedex-detail__title text-center mb-5">
+                {pokemon.name}
+                <span class="text-muted">#{pokemon.number}</span>
+              </h1>
+              <hr/>
+              <div class="row">
+                <div class="col-md-5">
+                  <img class="pokedex-detail__image" src={this.getUrlThumbnail}/>
+                </div>
+                <div class="col-md-7">
+                  <h4>Tipo</h4>
+                  {pokemonTags()}
+                  <h4 class="mt-3">Debilidades</h4>
+                  {weaknessTags()}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </DefaultLayout>
+    }
+  },
+  computed: {
+    getUrlThumbnail () {
+      let pokemon = this.$route.params.pokemon
+      return (!pokemon) ? '' : `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.number}.png`
+    }
+  }
+}
 </script>
-<style></style>
